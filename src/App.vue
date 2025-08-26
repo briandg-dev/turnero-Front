c3c93645edd890fe63d5c911170d87faa0c169e9
bdeguio <bdeguio@mav-sa.com.ar>
2025-08-26 10:56:25 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+import { RouterLink } from "vue-router";
+
+
+<template>
+  <header> 
+    <nav>
+      <router-link to="/">Home</router-link>
+    </nav>
+  </header>
+  
+  <router-view />
+</template>
+
+<style scoped></style>
